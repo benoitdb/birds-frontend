1fe55fa9625fc24795f448dbd578c6bd0eec32ff
Benoit Dejean <benoit.dejeandelabatie@gmail.com>
2021-06-03 13:26:31 +0200

    code ready to be deployed to production

--- setup.sh
+++ setup.sh
@@ -10,4 +10,11 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+[theme]\n\
+primaryColor = ‘#84a3a7’\n\
+backgroundColor = ‘#EFEDE8’\n\
+secondaryBackgroundColor = ‘#fafafa’\n\
+textColor= ‘#424242’\n\
+font = ‘sans serif’\n\
 " > ~/.streamlit/config.toml
+
